765df9d0fca3095a7ac17a91934254ed114a83c0
Nowwook <110366684+Nowwook@users.noreply.github.com>
2023-03-03 10:27:56 +0900

    Update IF.sql

--- Oracle/PL_SQL/IF.sql
+++ Oracle/PL_SQL/IF.sql
@@ -5,7 +5,7 @@ BEGIN
 
 IF SCORE >= 90 THEN                   -- 조건1
     DBMS_OUTPUT.PUT_LINE('A등급');    
-ELSIF SCORE >-80 THEN                 -- 조건2
+ELSIF SCORE >=80 THEN                 -- 조건2
     DBMS_OUTPUT.PUT_LINE('B등급');
 ELSE                                  -- 나머지
      DBMS_OUTPUT.PUT_LINE('C등급');
