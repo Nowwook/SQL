86d8329d2d7c4cf056e0d11c97ba0c640a805448
Nowwook <110366684+Nowwook@users.noreply.github.com>
2023-03-03 10:26:29 +0900

    Create IF.sql

---
+++ Oracle/PL_SQL/IF.sql
@@ -0,0 +1,14 @@
+DECLARE
+SCORE NUMBER :=80;  -- 변수선언
+
+BEGIN
+
+IF SCORE >= 90 THEN                   -- 조건1
+    DBMS_OUTPUT.PUT_LINE('A등급');    
+ELSIF SCORE >-80 THEN                 -- 조건2
+    DBMS_OUTPUT.PUT_LINE('B등급');
+ELSE                                  -- 나머지
+     DBMS_OUTPUT.PUT_LINE('C등급');
+END IF;                               -- if 종료 선언
+
+END;
