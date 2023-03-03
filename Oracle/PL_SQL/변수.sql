6a60e65381a80d5a9ad43e7bd602d474645c2377
Nowwook <110366684+Nowwook@users.noreply.github.com>
2023-03-03 09:39:46 +0900

    Update 변수.sql

--- Oracle/PL_SQL/변수.sql
+++ Oracle/PL_SQL/변수.sql
@@ -9,5 +9,5 @@ DECLARE NAME VARCHAR2(10) := '욱욱';
 --ex) 선언, 출력
 DECLARE NAME VARCHAR2(20) := '욱';
 BEGIN
-  DBMS_OUTPUT.put_Line('이효리'|| NAME); -- 출력
+  DBMS_OUTPUT.put_Line('nowuk'|| NAME); -- 출력
 END;
